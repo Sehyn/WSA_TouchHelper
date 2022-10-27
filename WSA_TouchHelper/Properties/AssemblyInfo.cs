4f2f910c41e4992c91196b721587a5d4b695ebcb
Arthur <arthur.bereslow.pro@gmail.com>
2022-10-27 21:12:40 +0200

    Added ico + several fixes

--- WSA_TouchHelper/Properties/AssemblyInfo.cs
+++ WSA_TouchHelper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WSA_TouchHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dofus Touch, but like 2.0 with hotkeys!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WSA_TouchHelper")]
