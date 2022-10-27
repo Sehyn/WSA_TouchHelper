532478a258366280405a6f343f9f17c52d1022e9
arthu <arthu@DEV>
2022-10-27 18:09:34 +0200

    Add project files.

---
+++ WSA_TouchHelper/Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WSA_TouchHelper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dreamForm1 = new ReaLTaiizor.Forms.DreamForm();
+            this.dreamTextBox1 = new ReaLTaiizor.Controls.DreamTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dreamTextBox2 = new ReaLTaiizor.Controls.DreamTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dreamForm1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dreamForm1
+            // 
+            this.dreamForm1.ColorA = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.dreamForm1.ColorB = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.dreamForm1.ColorC = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.dreamForm1.ColorD = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.dreamForm1.ColorE = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dreamForm1.ColorF = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dreamForm1.Controls.Add(this.label3);
+            this.dreamForm1.Controls.Add(this.label2);
+            this.dreamForm1.Controls.Add(this.dreamTextBox2);
+            this.dreamForm1.Controls.Add(this.label1);
+            this.dreamForm1.Controls.Add(this.dreamTextBox1);
+            this.dreamForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dreamForm1.Location = new System.Drawing.Point(0, 0);
+            this.dreamForm1.Name = "dreamForm1";
+            this.dreamForm1.Size = new System.Drawing.Size(341, 155);
+            this.dreamForm1.TabIndex = 0;
+            this.dreamForm1.TabStop = false;
+            this.dreamForm1.Text = "WSA_Touch";
+            this.dreamForm1.TitleAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dreamForm1.TitleHeight = 25;
+            // 
+            // dreamTextBox1
+            // 
+            this.dreamTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.dreamTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dreamTextBox1.ColorA = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.dreamTextBox1.ColorB = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.dreamTextBox1.ColorC = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.dreamTextBox1.ColorD = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dreamTextBox1.ColorE = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dreamTextBox1.ColorF = System.Drawing.Color.Black;
+            this.dreamTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.dreamTextBox1.Location = new System.Drawing.Point(86, 31);
+            this.dreamTextBox1.Name = "dreamTextBox1";
+            this.dreamTextBox1.Size = new System.Drawing.Size(63, 20);
+            this.dreamTextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sort #1:";
+            // 
+            // dreamTextBox2
+            // 
+            this.dreamTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.dreamTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dreamTextBox2.ColorA = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.dreamTextBox2.ColorB = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.dreamTextBox2.ColorC = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.dreamTextBox2.ColorD = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dreamTextBox2.ColorE = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dreamTextBox2.ColorF = System.Drawing.Color.Black;
+            this.dreamTextBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.dreamTextBox2.Location = new System.Drawing.Point(86, 59);
+            this.dreamTextBox2.Name = "dreamTextBox2";
+            this.dreamTextBox2.Size = new System.Drawing.Size(63, 20);
+            this.dreamTextBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(64, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(64, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 155);
+            this.Controls.Add(this.dreamForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.dreamForm1.ResumeLayout(false);
+            this.dreamForm1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.DreamForm dreamForm1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private ReaLTaiizor.Controls.DreamTextBox dreamTextBox2;
+        private System.Windows.Forms.Label label1;
+        private ReaLTaiizor.Controls.DreamTextBox dreamTextBox1;
+    }
+}
+
---
+++ WSA_TouchHelper/Main.cs
@@ -0,0 +1,86 @@
+﻿using MouseKeyboardLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSA_TouchHelper
+{
+   
+    public partial class Main : Form
+    {
+       
+
+        private KeyboardHook _keyboardHook;
+      
+        public Main()
+        {
+            InitializeComponent();
+            InitializeKeyboardHook(); //calls the method beneath to init the Hook
+
+        }
+        private void InitializeKeyboardHook()
+        {
+            _keyboardHook = new KeyboardHook(); //initialize
+            _keyboardHook.KeyUp += new KeyEventHandler(HandleKeyUp); //assign event: when a key gets pressed, call this method
+            _keyboardHook.Start();
+        }
+
+        private void HandleKeyUp(object sender, KeyEventArgs e)
+        {
+            //this gets called automatically by the KeyboardHook when ANY key is released
+            // e contains the event information
+            var pressedKey = e.KeyCode;
+
+            /*
+            It would be better practice to use a "switch statement" (https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/switch)
+            instead of if and else here, but I kept it like that.
+            */
+
+            if (pressedKey == Keys.F9)
+            {
+                Utilities.LoggerInfo("F9 -> Showing Window");
+        
+                IntPtr hWnd = Native.FindWindow(null, "DOFUS Touch");
+
+                ForegroundWindowBypass.Set(hWnd);
+
+            }
+            if (pressedKey == Keys.F2)
+            {
+                Utilities.LoggerInfo("F2 -> Clicking to");
+
+                IntPtr hWnd = Native.FindWindow(null, "DOFUS Touch");
+
+                ForegroundWindowBypass.Set(hWnd);
+                Native.PostMessage(hWnd, Native.WM_LBUTTONDOWN, 1, Native.MakeLParam(Convert.ToInt32(dreamTextBox1.Text), Convert.ToInt32(dreamTextBox2.Text)));
+                Native.PostMessage(hWnd, Native.WM_LBUTTONUP, 0, Native.MakeLParam(Convert.ToInt32(dreamTextBox1.Text), Convert.ToInt32(dreamTextBox2.Text)));
+
+
+            }
+            if (pressedKey == Keys.F3)
+            {
+                Utilities.LoggerInfo("F3 -> CursorPos:" );
+
+                IntPtr hWnd = Native.FindWindow(null, "DOFUS Touch");
+
+                ForegroundWindowBypass.Set(hWnd);
+                Native.POINT lpPoint;
+                Native.GetCursorPos(out lpPoint);
+                Native.ScreenToClient(hWnd, ref lpPoint);
+                Console.WriteLine("X : " + lpPoint.X);
+                Console.WriteLine("Y : " + lpPoint.Y);
+
+            }
+
+        }
+    }
+}
